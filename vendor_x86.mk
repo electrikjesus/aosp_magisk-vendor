267059a9609db2087d09e27ff18fba95a9f5a81d
electrikjesus <electrikjesus@gmail.com>
2017-05-13 19:49:18 -0400

    Start work for x86

--- vendor_x86.mk
+++ vendor_x86.mk
@@ -1,4 +1,14 @@
 PRODUCT_COPY_FILES += \
-    vendor/magisk/common/install-recovery.sh:system/etc/install-recovery.sh 
+    vendor/magisk/common/init.magisk.rc:system/init.magisk.rc \
+    vendor/magisk/common/custom_ramdisk_patch.sh:system/bin/custom_ramdisk_patch.sh \
+    vendor/magisk/common/magic_mask.sh:system/bin/magic_mask.sh \
+    vendor/magisk/x86/su:system/xbin/su \
+    vendor/magisk/x86/su:system/bin/.ext/.su \
+    vendor/magisk/x86/su:system/xbin/daemonsu \
+    vendor/magisk/x86/magiskpolicy:system/xbin/magiskpolicy \
+    vendor/magisk/x86/magiskboot:system/xbin/magiskboot \
+    vendor/magisk/x86/resetprop:system/xbin/resetprop \
+    vendor/magisk/x86/magiskhide:system/xbin/magiskhide \
+    vendor/magisk/x86/busybox:system/xbin/busybox \
 
 PRODUCT_PACKAGES += Magisk
